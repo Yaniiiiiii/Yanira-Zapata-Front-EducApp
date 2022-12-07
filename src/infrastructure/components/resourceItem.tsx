77349323f49c2d9d38ba872618d34812cfade69d
Yanira <yanirazapata.c@gmail.com>
2022-12-07 20:17:02 +0100

    🔥 remove files

--- src/infrastructure/components/resourceItem.tsx
+++
@@ -1,9 +0,0 @@
-import { Resource } from '../services/types/resources.types';
-
-export function ResourceItem({ resource }: { item: Resource }) {
-    return (
-        <li>
-            <input type={'file'}></input>
-        </li>
-    );
-}
