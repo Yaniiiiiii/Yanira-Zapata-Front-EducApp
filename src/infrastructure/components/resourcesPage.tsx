842f73a321a5843e33349dc7b0537afac45050a8
Yanira <yanirazapata.c@gmail.com>
2022-12-07 20:19:56 +0100

    🔥 delete failing test

--- src/infrastructure/components/resourcesPage.tsx
+++ src/infrastructure/components/resourcesPage.tsx
@@ -10,3 +10,35 @@ function ResourcesPage() {
 }
 
 export default ResourcesPage;
+
+import { Resource } from '../services/types/resources.types';
+
+export function ResourceItem({ resource }: { item: Resource }) {
+    return (
+        <li>
+            <input type={'file'}></input>
+        </li>
+    );
+}
+
+import { useResources } from '../hooks/useResources';
+import { Resource } from '../services/types/resources.types';
+
+export function ResourcesList() {
+    const title = 'Resources';
+    const { resources, handleLoad } = useResources();
+    console.log(resources);
+    return resources.length ? (
+        <section>
+            <p>{resources[0].title}</p>
+            <h2>{title}</h2>
+            <ul className="resourcesList">
+                {resources.map((item: Resource) => (
+                    <ResouceItem key={item.id} item={item}></ResouceItem>
+                ))}
+            </ul>
+        </section>
+    ) : (
+        <div>Tal</div>
+    );
+}
