4e1225a43a59825184266efd54fe490feb6b5698
Yanira <yanirazapata.c@gmail.com>
2022-12-09 13:33:57 +0100

    ✅ update login test

--- src/infrastructure/components/userLogin/userLogin.test.tsx
+++ src/infrastructure/components/userLogin/userLogin.test.tsx
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { fireEvent, render, renderHook, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { Provider } from 'react-redux';
-import { mockUser } from '../../../mocks/mocks';
 import { rootState, rootStore } from '../../../store/store';
-import { useUsers } from '../../hooks/useUsers';
 import { resourceReducer } from '../../reducer/resourceReducer';
 import { usersReducer } from '../../reducer/usersReducer';
 import { UserLogin } from './userLogin';
@@ -37,11 +35,11 @@ describe('Given the user Login  component', () => {
                 </Provider>
             );
 
+            const formInput = screen.getAllByRole('textbox');
+            fireEvent.change(formInput[0], { target: { value: 'test' } });
             const element = screen.getByRole('button');
             userEvent.click(element);
-            const formInput = screen.getByRole('textbox');
-            fireEvent.change(formInput, { target: { value: 'test' } });
-            expect(formInput).toBeInTheDocument();
+            expect(formInput[0]).toBeInTheDocument();
         });
 
         // test('Then it should render the logIn form', () => {
