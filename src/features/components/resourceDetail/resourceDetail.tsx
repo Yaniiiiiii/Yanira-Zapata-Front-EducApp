512d33ff11bac3b2461e73dbba7944bb946acf01
Yanira <yanirazapata.c@gmail.com>
2022-12-14 13:42:19 +0100

    💄  update details CSS

--- src/features/components/resourceDetail/resourceDetail.tsx
+++ src/features/components/resourceDetail/resourceDetail.tsx
@@ -59,27 +59,39 @@ export function ResourceDetails() {
     return (
         <>
             <Header />
-            <li className={style.resourceLi}>
-                <h2 className={style.resourceItem}>{details.title}</h2>
-                <p className={style.by}>by</p>
-                <p className={style.p}>{details.owner?.name}</p>
-                <p className={style.p}>{details.pages}</p>
-                <p className={style.subject}>{details.subject}</p>
-                <p className={style.p}>{details.grade}</p>
-                <p className={style.subject}>{details.description}</p>
-                <img src="https://opendocs.com/wp-content/uploads/2019/10/Blank-Simple-Invoice.pdf"></img>
+            <section className={style.resourceSection}>
+                <li className={style.resourceLi}>
+                    <h2 className={style.resourceItem}>{details.title}</h2>
+                    <p className={style.by}>by</p>
+                    <p className={style.p}>{details.owner?.name}</p>
 
-                <button className={style.like} onClick={handleLike}>
-                    <img src="/assets/fav.jpg"></img>
-                </button>
-                <button className={style.edit} onClick={handleEdit}>
-                    <img src="/assets/buy.jpg"></img>
-                </button>
-                <button
-                    className={style.delete}
-                    onClick={handleRemove}
-                ></button>
-            </li>
+                    <div className={style.divResource}>
+                        {' '}
+                        <button className={style.like} onClick={handleLike}>
+                            ♡
+                        </button>
+                        <button className={style.edit} onClick={handleEdit}>
+                            ✎
+                        </button>
+                        <button className={style.delete} onClick={handleRemove}>
+                            🛒
+                        </button>
+                        <img
+                            width="330px"
+                            src="/assets/logo.jpg"
+                            className={style.img}
+                        ></img>
+                    </div>
+                    <div className={style.subjectGrade}>
+                        {' '}
+                        <p className={style.subject}>{details.subject}</p>
+                        <p className={style.p}>- {details.grade}</p>
+                    </div>
+
+                    <p className={style.descriptionTitle}>Description</p>
+                    <p className={style.description}>{details.description}</p>
+                </li>
+            </section>
             <Footer />
         </>
     );
--- src/features/components/resourceItem/resourceItem.tsx
+++ src/features/components/resourceItem/resourceItem.tsx
@@ -4,28 +4,20 @@ import style from './resourceItem.module.css';
 
 export function ResourceItem({ item }: { item: Resource }) {
     return (
-        <div>
-            <li className={style.resource}>
-                <Link
-                    to={'/resources/' + item.id}
-                    key={item.id}
-                    className={style.divFormat}
-                >
-                    <div className={style.iframeContainer}>
-                        <iframe
-                            className={style.iframe}
-                            src={item.format}
-                        ></iframe>
-                    </div>
-                    <div>
-                        <p>{item.subject}</p>
-                    </div>
-                    <div>
-                        <p>{item.grade}</p>
-                    </div>
-                </Link>
-            </li>
-        </div>
+        <li className={style.resource}>
+            <Link to={'/resources/' + item.id} key={item.id}>
+                <div className={style.divFormat}>
+                    {' '}
+                    <img width="150" src="/assets/logo.jpg"></img>
+                </div>
+            </Link>
+            <div>
+                <p>{item.subject}</p>
+            </div>
+            <div>
+                <p>{item.grade}</p>
+            </div>
+        </li>
     );
 }
 export default ResourceItem;
